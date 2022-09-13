7229bd75d0573ca6c848e74c751aa43c753dbe6b
AdityaKshettri <adikshettri1623@gmail.com>
2022-09-13 15:26:28 +0530

    added video player logic

--- app/src/main/java/com/aditya/project/MainActivity.kt
+++ app/src/main/java/com/aditya/project/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.aditya.project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeVideoPlayer()
+    }
+
+    private fun initializeVideoPlayer() {
+        val videoView: VideoView = findViewById(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.video)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 }
